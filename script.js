e2a394a4875cef1fd8b54c7651f601b235715877
OJ-Design <155101051+OJ-Design@users.noreply.github.com>
2023-12-28 22:21:36 +0000

    Update script.js

--- script.js
+++ script.js
@@ -6,4 +6,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Scroll to the 'index' section
     document.getElementById('index').scrollIntoView();
+
+    // Smooth scroll for anchor links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    });
 });
