87d2e0c3bf7664e99719d974c40ff1db7439261b
OJ-Design <155101051+OJ-Design@users.noreply.github.com>
2023-12-28 23:19:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   var backToTopButton = document.getElementById("back-to-top");
+  var indexSection = document.getElementById("index");
 
   window.addEventListener("scroll", function () {
-    if (window.scrollY > 200) {
+    var scrollPosition = window.scrollY;
+    var indexSectionBottom = indexSection.offsetTop + indexSection.offsetHeight;
+
+    if (scrollPosition > indexSectionBottom) {
       backToTopButton.style.display = "block";
     } else {
       backToTopButton.style.display = "none";
