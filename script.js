9d2b9550c895cb842a2a735d1c5be84291d67420
OJ-Design <155101051+OJ-Design@users.noreply.github.com>
2023-12-28 22:38:58 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var backToTopButton = document.getElementById("back-to-top");
 
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 200) {
+      backToTopButton.style.display = "block";
+    } else {
+      backToTopButton.style.display = "none";
+    }
+  });
+});
