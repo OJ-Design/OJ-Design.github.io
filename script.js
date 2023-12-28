a292805ff48845517af39ace26dd1cd18fdfbc1d
OJ-Design <155101051+OJ-Design@users.noreply.github.com>
2023-12-28 22:55:52 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var backToTopButton = document.getElementById("back-to-top");
+  var backToTopButton = document.getElementById("back-to-top");
 
-    window.addEventListener("scroll", function () {
-        if (window.scrollY > 200) {
-            backToTopButton.style.display = "block";
-        } else {
-            backToTopButton.style.display = "none";
-        }
-    });
-
-    // Add a click event listener to scroll to the top of the page
-    backToTopButton.addEventListener("click", function () {
-        window.scrollTo({ top: 0, behavior: "smooth" });
-    });
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 200) {
+      backToTopButton.style.display = "block";
+    } else {
+      backToTopButton.style.display = "none";
+    }
+  });
 });
